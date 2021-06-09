dac0ab0b76ec3d3914068a9f6c44f570547038dd
William <capuletos@live.com>
2021-06-09 18:37:45 -0300

    Exibindo dados do construtor e ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myDay';
+  public tasks: any[] = [];
+  public title: String = 'My Days';
+  /**Testando lista de inpunt no task
+   */
+  constructor() {
+    this.tasks.push('1, Concluir o estudor de variaveis');
+    this.tasks.push('2, Falta finalizar algo');
+    this.tasks.push({message: 'Testando'});
+    this.tasks.push(new Date());
+    
+  }
+  changeText(){
+    this.title = 'Texto foi alterado';
+  }
+  // title = 'myDay';
 }
